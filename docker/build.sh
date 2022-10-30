fc5f142da4b49ba68a6ee9dce8aeb7a3047dea10
Kseniia Churiumova <kchuriumova@paypal.com>
2022-10-30 10:43:26 -0400

    Use script

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,6 @@
-GOOS=linux GOARCH=amd64 time go build -o ./bin/intake-linux64 ./cmd/intake
-GOOS=linux GOARCH=amd64 time go build -o ./bin/deliver-linux64 ./cmd/deliver
-GOOS=linux GOARCH=amd64 time go build -o ./bin/validate-linux64 ./cmd/validate
+echo "Building intake..."
+GOOS=linux GOARCH=amd64 time go build -o ../bin/intake-linux64 ../cmd/intake
+echo "Building deliver..."
+GOOS=linux GOARCH=amd64 time go build -o ../bin/deliver-linux64 ../cmd/deliver
+echo "Building validate..."
+GOOS=linux GOARCH=amd64 time go build -o ../bin/validate-linux64 ../cmd/validate
