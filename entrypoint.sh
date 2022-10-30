05cae37f013c6e379a8101ceb1c066c39d13ef9a
Kseniia Churiumova <kchuriumova@paypal.com>
2022-10-30 11:16:32 -0400

    remove k8s config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,28 +2,24 @@
 
 if [ "$1" = "intake" ]; 
 then
-	eval "echo \"$(cat /AdKS/k8s/config)\"" > /AdKS/k8s/config
 	/AdKS/bin/intake-linux64
 	exit 0
 fi
 
 if [ "$1" = "deliver" ];
 then 
-	eval "echo \"$(cat /AdKS/k8s/config)\"" > /AdKS/k8s/config
 	/AdKS/bin/deliver-linux64
 	exit 0
 fi
 
 if [ "$1" = "validate" ];
 then 
-	eval "echo \"$(cat /AdKS/k8s/config)\"" > /AdKS/k8s/config
 	/AdKS/bin/validate-linux64
 	exit $?
 fi
 
 if [ "$1" = "validate-targets" ];
 then 
-	eval "echo \"$(cat /AdKS/k8s/config)\"" > /AdKS/k8s/config
 	/AdKS/bin/validate-targets-linux64
 	exit $?
 fi
